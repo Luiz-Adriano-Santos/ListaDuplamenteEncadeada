31f6993a82d4cb5c70dffd79854c53bf607b7418
AndresKindel <andreskbarbosa@outlook.com>
2024-04-20 20:31:12 -0300

    add atributo chave

--- caixa.py
+++ caixa.py
@@ -1,14 +1,24 @@
 class Caixa:
-    def __init__(self,valor):
+    def __init__(self, valor):
+        self.__chave = id(self)
         self.__valor = valor
         self.__proximo = None
         self.__anterior = None
 
+    def chave(self):
+        return self.__chave
+
     def valor(self):
         return self.__valor
-    
+
     def proximo(self):
         return self.__proximo
-    
+
     def anterior(self):
         return self.__anterior
+
+    def set_proximo(self, proximo):
+        self.__proximo = proximo
+
+    def set_anterior(self, anterior):
+        self.__anterior = anterior
